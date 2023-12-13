d0483b3fc3ef5dc57d4dd115a9c563175f09d5bd
James Weatherill <weatherill.james@icloud.com>
2023-12-13 16:52:44 +0000

    Formatting

--- Day5/Day5Pt2.py
+++ Day5/Day5Pt2.py
@@ -7,6 +7,18 @@ startTime = time()
 ########################################
 
 fixedList = []
+file1 = open("input.txt", "r")
+file2 = open("testInput.txt", "r")
+fileReadList = str(file1.readlines()).replace("\\n', '", ".").replace("..", "|").replace("['", "").replace("\\n']", "").replace(" map:", "").split("|")
+for string in fileReadList:
+    fixedList.append(string.split("."))
+fixedList[0] = str(fixedList[0]).replace("['", "").replace("']", "").split(" ")
+for i in range(1, len(fixedList)):
+    for j in range(len(fixedList[i])):
+        fixedList[i][j] = fixedList[i][j].split(" ")
+
+########################################
+
 seeds = fixedList[0][1:]
 seed2Soil = fixedList[1][1:]
 soil2Fertilizer = fixedList[2][1:]
@@ -18,16 +30,6 @@ humidity2Location = fixedList[7][1:]
 
 exchangeList = [seeds, seed2Soil, soil2Fertilizer, fertilizer2Water, water2Light, light2Temperature, temerature2Humidity, humidity2Location]
 
-file1 = open("input.txt", "r")
-file2 = open("testInput.txt", "r")
-fileReadList = str(file1.readlines()).replace("\\n', '", ".").replace("..", "|").replace("['", "").replace("\\n']", "").replace(" map:", "").split("|")
-for string in fileReadList:
-    fixedList.append(string.split("."))
-fixedList[0] = str(fixedList[0]).replace("['", "").replace("']", "").split(" ")
-for i in range(1, len(fixedList)):
-    for j in range(len(fixedList[i])):
-        fixedList[i][j] = fixedList[i][j].split(" ")
-
 for i in range(len(seeds)):
     seeds[i] = int(seeds[i])
 
@@ -35,10 +37,6 @@ for i in range(1, len(exchangeList)):
     for j in range(len(exchangeList[i])):
         for k in range(len(exchangeList[i][j])):
             exchangeList[i][j][k] = int(exchangeList[i][j][k])
-
-########################################
-
-
 
 ########################################
 
